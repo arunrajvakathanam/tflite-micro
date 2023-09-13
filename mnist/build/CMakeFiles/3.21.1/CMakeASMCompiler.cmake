38216a1622c04f700eed715c557a9c88161d1abe
arunrajvakathanam <arunrajvakathanam@gmail.com>
2023-09-13 18:17:05 +0530

    New mnist Digit

---
+++ mnist/build/CMakeFiles/3.21.1/CMakeASMCompiler.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_ASM_COMPILER "/home/arun/zephyr-sdk-0.16.1/x86_64-zephyr-elf/bin/x86_64-zephyr-elf-gcc")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "/home/arun/zephyr-sdk-0.16.1/x86_64-zephyr-elf/bin/x86_64-zephyr-elf-ar")
+set(CMAKE_ASM_COMPILER_AR "/home/arun/zephyr-sdk-0.16.1/x86_64-zephyr-elf/bin/x86_64-zephyr-elf-gcc-ar")
+set(CMAKE_RANLIB "/home/arun/zephyr-sdk-0.16.1/x86_64-zephyr-elf/bin/x86_64-zephyr-elf-ranlib")
+set(CMAKE_ASM_COMPILER_RANLIB "/home/arun/zephyr-sdk-0.16.1/x86_64-zephyr-elf/bin/x86_64-zephyr-elf-gcc-ranlib")
+set(CMAKE_LINKER "/home/arun/zephyr-sdk-0.16.1/x86_64-zephyr-elf/bin/../lib/gcc/x86_64-zephyr-elf/12.2.0/../../../../x86_64-zephyr-elf/bin/ld.bfd")
+set(CMAKE_MT "")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
---
+++ mnist/build/zephyr/zephyr.dts.d
@@ -0,0 +1,8 @@
+empty_file.o: /home/arun/zephyrproject/zephyr/misc/empty_file.c \
+ /home/arun/zephyrproject/zephyr/boards/x86/qemu_x86/qemu_x86_64.dts \
+ /home/arun/zephyrproject/zephyr/boards/x86/qemu_x86/qemu_x86.dts \
+ /home/arun/zephyrproject/zephyr/dts/common/mem.h \
+ /home/arun/zephyrproject/zephyr/dts/x86/intel/ia32.dtsi \
+ /home/arun/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /home/arun/zephyrproject/zephyr/include/zephyr/dt-bindings/interrupt-controller/intel-ioapic.h \
+ /home/arun/zephyrproject/zephyr/include/zephyr/dt-bindings/pcie/pcie.h
